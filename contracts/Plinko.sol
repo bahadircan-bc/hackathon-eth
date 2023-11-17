8943b12e6898a8e91ea4f3982cbfa9071dc151c7
Csonuzun <Candassonuzun@gmail.com>
2023-11-18 02:18:14 +0300

    Update Plinko & Troubles with redstone

--- contracts/Plinko.sol
+++ contracts/Plinko.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Plinko {
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    struct Bet {
-        address player;
-        uint256 wager;
-        uint256 multipleBets;
-        uint8 rows;
-        uint8 risk;
-    }
+contract Plinko is ReentrancyGuard, Ownable {
+    using SafeERC20 for IERC20;
+
+    mapping(uint8 => mapping(uint8 => uint256[])) public plinkoMultipliers;
+    mapping(uint256 => Bet) private games;
 
     constructor(){
+        token = _token;
+    }
 
+    function getMultipliers(uint8 risk, uint8 numRows)
+    external
+    view
+    returns (uint256[] memory multipliers)
+    {
+        return plinkoMultipliers[risk][numRows];
     }
 }
